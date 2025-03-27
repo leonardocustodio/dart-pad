f0c9c24522c0ec30d601a242e0f3df6458c226a8
Leonardo Custodio <leonardo@custodio.me>
2025-03-27 13:09:54 +0100

    changes

--- pkgs/dartpad_ui/lib/samples.g.dart
+++ pkgs/dartpad_ui/lib/samples.g.dart
@@ -57,10 +57,21 @@ const _dart = Sample(
   name: 'Dart snippet',
   id: 'dart',
   source: r'''
+// Copyright 2025 to the Polkadart Team
+import 'package:polkadart_scale_codec/polkadart_scale_codec.dart';
+
 void main() {
-  for (var i = 0; i < 10; i++) {
-    print('hello ${i + 1}');
-  }
+  // Encode
+  var output = HexOutput();
+  final value = 69;
+  CompactCodec.codec.encodeTo(value, output);
+  var encodedHex = output.toString();
+  print(encodedHex); // 0x1501
+
+  // Decode
+  var input = Input.fromHex(encodedHex);
+  var decoded = CompactCodec.codec.decode(input);
+  print(decoded); // 69
 }
 ''',
 );
