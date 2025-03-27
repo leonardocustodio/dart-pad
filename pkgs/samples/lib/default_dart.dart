75361d0f2306b4c555efb8d58c6dea08dc4d4625
Leonardo Custodio <leonardo@custodio.me>
2025-03-27 13:09:39 +0100

    changes

--- pkgs/samples/lib/default_dart.dart
+++ pkgs/samples/lib/default_dart.dart
@@ -1,9 +1,16 @@
-// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright 2025 to the Polkadart Team
+import 'package:polkadart_scale_codec/polkadart_scale_codec.dart';
 
 void main() {
-  for (var i = 0; i < 10; i++) {
-    print('hello ${i + 1}');
-  }
+  // Encode
+  var output = HexOutput();
+  final value = 69;
+  CompactCodec.codec.encodeTo(value, output);
+  var encodedHex = output.toString();
+  print(encodedHex); // 0x1501
+
+  // Decode
+  var input = Input.fromHex(encodedHex);
+  var decoded = CompactCodec.codec.decode(input);
+  print(decoded); // 69
 }
